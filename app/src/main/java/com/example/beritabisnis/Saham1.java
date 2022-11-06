8e8da430a05c0b143fa3014822ba065584e18732
mry88 <yusufmuhammadrizal@gmail.com>
2022-11-06 20:40:23 +0700

    tugas5_2018013

---
+++ app/src/main/java/com/example/beritabisnis/Saham1.java
@@ -0,0 +1,14 @@
+package com.example.beritabisnis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Saham1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saham1);
+    }
+}
